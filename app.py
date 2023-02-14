a62fba66739215b07e318b268ebdc48ff6e15f42
Jennifer Guo <jenniferguo@college.harvard.edu>
2023-02-14 18:37:42 -0500

    label concepts

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# SQLite Database concept
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -11,36 +13,43 @@ class Todo(db.Model):
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
-
+# App Route Decorator Concept
 @app.route('/')
 def index():
-    # show all todos
     todo_list = Todo.query.all()
+    # Reusable Components/Templates Concept
     return render_template('base.html', todo_list=todo_list)
 
+# App Route Decorator Concept
 @app.route("/add", methods=["POST"])
 def add():
     with app.app_context():
+        # HTTP Request Concept
         title = request.form.get("title")
         new_todo = Todo(title=title, complete=False)
         db.session.add(new_todo)
         db.session.commit()
+        # Redirect Concept
         return redirect(url_for("index"))
 
+# App Route Decorator Concept
 @app.route("/update/<int:todo_id>")
 def update(todo_id):
     with app.app_context():
         todo = Todo.query.filter_by(id=todo_id).first()
         todo.complete = not todo.complete
         db.session.commit()
+        # Redirect Concept
         return redirect(url_for("index"))
 
+# App Route Decorator Concept
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
     with app.app_context():
         todo = Todo.query.filter_by(id=todo_id).first()
         db.session.delete(todo)
         db.session.commit()
+        # Redirect Concept
         return redirect(url_for("index"))
 
 if __name__ == "__main__":
